1521e508f6d4ef870495195fcc7fd81886b0c89b
Brian McCallister <brianm@skife.org>
2011-12-26 22:04:55 -0700

    merge in master

--- sys-wordpress.rb
+++ sys-wordpress.rb
@@ -1,5 +1,6 @@
 
 wp_url = "http://wordpress.org/wordpress-3.2.1.tar.gz"
+mcp_url = "http://downloads.wordpress.org/plugin/memcached.2.0.1.zip"
 
 system "blog" do
   
@@ -11,9 +12,22 @@ system "blog" do
     cardinality: 2,
     base: "apache-server",
     install: ["tgz:#{wp_url}?to=/var/www/&skiproot=wordpress",
+              "zip:#{mcp_url}?to=/var/www/wp-content/&skiproot=memcached",
+              "exec: yes | sudo pecl install memcache",
+              "exec: sudo echo 'extension=memcache.so' >> /etc/php5/apache2/php.ini",
               "wait-for:wordpress-db",
-              "erb:wp-config.php.erb > /var/www/wp-config.php",
+              "erb: wp-config.php.erb > /var/www/wp-config.php",            
+              "exec: sudo service apache2 restart",
               "elb-add:blog"]
+  }
+
+  server "memcached", {
+    cardinality: 2,
+    base: "server",
+    install: ["scratch:memcached=@", 
+              "apt:memcached",
+              "file:memcached.conf > /etc/memcached.conf",
+              "exec: sudo service memcached restart"]
   }
 
   server "database", {
