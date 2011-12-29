064535a6322583ea42ae30ad74f741f21c31e1f5
Brian McCallister <brianm@skife.org>
2011-12-28 20:18:54 -0700

    revert a bad checkin

--- sys-wordpress.rb
+++ sys-wordpress.rb
@@ -1,5 +1,6 @@
 
 wp_url = "http://wordpress.org/wordpress-3.2.1.tar.gz"
+mcp_url = "http://downloads.wordpress.org/plugin/memcached.2.0.1.zip"
 
 system "blog" do
   
@@ -11,11 +12,11 @@ system "blog" do
     cardinality: 2,
     base: "apache-server",
     install: ["tgz:#{wp_url}?to=/var/www/&skiproot=wordpress",
-              "file:wordpress/object-cache.php > /var/www/wp-content/object-cache.php",
+              "zip:#{mcp_url}?to=/var/www/wp-content/&skiproot=memcached",
               "exec: yes | sudo pecl install memcache",
               "exec: sudo echo 'extension=memcache.so' >> /etc/php5/apache2/php.ini",
               "wait-for:wordpress-db",
-              "erb: wordpress/wp-config.php.erb > /var/www/wp-config.php",
+              "erb: wp-config.php.erb > /var/www/wp-config.php",            
               "exec: sudo service apache2 restart",
               "elb-add:blog"]
   }
@@ -25,7 +26,7 @@ system "blog" do
     base: "server",
     install: ["scratch:memcached=@", 
               "apt:memcached",
-              "file:memcached/memcached.conf > /etc/memcached.conf",
+              "file:memcached.conf > /etc/memcached.conf",
               "exec: sudo service memcached restart"]
   }
 
